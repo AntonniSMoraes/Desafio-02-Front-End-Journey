7907434097682d48a75c6a1f351a0f74d5717a3e
AntonniSMoraes <antonnimoraes@gmail.com>
2023-07-13 22:45:11 -0300

    Seletores universais removidos do header. Adicionada parte da main page.

---
+++ src/main-page/Main.jsx
@@ -0,0 +1,10 @@
+import "./Main.css";
+import SectionOne from "./SectionOne";
+
+export default function Main() {
+  return (
+    <body>
+      <SectionOne />
+    </body>
+  );
+}
---
+++ src/main-page/SectionOne.jsx
@@ -0,0 +1,46 @@
+import "./SectionOne.css";
+import heroAnimation from "../res/heroanimation.gif";
+
+export default function SectionOne() {
+  return (
+    <section className="first-pg">
+      <div className="organizer">
+        <div className="first-pg-txt">
+          <h1 id="title">Discover Digital Art & Collect Nfts</h1>
+          <p id="text">
+            NFT marketplace UI created with Anima for Figma. Collect, buy and
+            sell art from more than 20k NFT artists.
+          </p>
+        </div>
+        {/* botão para artist page */}
+        <a href="/">
+          <img src={heroAnimation} className="first-pg-gif" />
+        </a>
+      </div>
+      <div className="organizer-two">
+        {/* botão para sign up */}
+        <button className="get-started">
+          <img
+            id="rocket"
+            src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/rocketlaunch@2x.svg"
+          />
+          Get Started!
+        </button>
+        <div className="info-pg">
+          <div>
+            <p id="aditional-value">240k+</p>
+            <p id="aditional-info">Total Sale</p>
+          </div>
+          <div>
+            <p id="aditional-value">100k+</p>
+            <p id="aditional-info">Auctions</p>
+          </div>
+          <div>
+            <p id="aditional-value">240k+</p>
+            <p id="aditional-info">Artists</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
