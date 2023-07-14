0a78bc27b2cbab439f9c175e54a3523384edc888
Tomas Benfica <tomasrbenfica@outlook.com>
2023-07-14 12:22:13 -0300

    Inserção de URLs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Marketplace from './marketplace/Marketplace';
 import Footer from './commons/Footer';
 
 export default function App() {
+
+  const ARTISTS_URL = 'https://nftdatabase-703d2-default-rtdb.firebaseio.com/artists.json';
+  const NFTS_URL = 'https://nftdatabase-703d2-default-rtdb.firebaseio.com/nfts.json';
+
     return (
       <div>
         <Header />
